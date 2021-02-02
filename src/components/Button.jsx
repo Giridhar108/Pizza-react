1e9a34beecf5a6e0b6fc20b1f0028b1ce4ff3535
German Fomenko <pentagram777@mail.ru>
2021-02-02 09:06:21 +0500

    add button

---
+++ src/components/Button.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import classNames from "classnames";
+
+function Button({onClick, className, outline, children} /* props */) {
+
+  // const {onClick, className, outline, children} = props
+
+  return (
+    <button onClick={onClick}
+    className={classNames("button", className, {
+      "button--outline": outline,
+    })}
+  >
+    {children}
+  </button>
+  )
+}
+
+/*
+class Button extends React.Component {
+  // componentDidMount() {
+  //   console.log('i am here')
+  // }
+  render() {
+    return (
+      <button
+        className={classNames("button", {
+          "button--outline": this.props.outline,
+        })}
+      >
+        {this.props.children}
+      </button>
+    );
+  }
+  // render() {
+  //   return <button className={`button ${this.props.outline ? 'button--outline' : ''}`} >{this.props.children}</button>
+  // }
+}
+ */
+
+export default Button;
